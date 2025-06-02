89f2146f19008d139e85bb39f47e59fb2637fb7a
Tony King <a.korolev9927@gmail.com>
2025-06-02 20:11:08 +0300

    Нужно создать приложение с 2-мя горутинами, где: - Первая создаёт slice из 10 случайных элементов от 0 до 100 и передаёт их по одному во вторую горутину. - Вторая получает числа от 1-й и возводит в квадрат передавая результат в main. - В main дожидаемся всех 10 чисел, которые были возведены в квадрат и выводим их в консоль.

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func randomInt(chRandom chan<- int) {
+	defer close(chRandom)
+	arr := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		arr[i] = rand.IntN(101)
+		chRandom <- arr[i]
+	}
+}
+
+func power(chRandom <-chan int, chPow chan<- int) {
+	defer close(chPow)
+	for num := range chRandom {
+		chPow <- num * num
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	chRandom := make(chan int)
+	chPower := make(chan int)
+
+	go randomInt(chRandom)
+	go power(chRandom, chPower)
+
+	for num := range chPower {
+		fmt.Println(num)
+	}
 }
