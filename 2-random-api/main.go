913f08fb91834d3e68cee23dcebf4b9ed32134c3
Tony King <a.korolev9927@gmail.com>
2025-06-02 20:38:08 +0300

    Сделать API с одним обработчиком, который при запросе возвращает случайное число от 1 до 6. - Для возврата использовать

---
+++ 2-random-api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"net/http"
+)
+
+type MyMux struct{}
+
+func (mux *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+		w.Write([]byte(fmt.Sprintf("%d", rand.IntN(7))))
+
+	}
+
+}
+func main() {
+
+	router := &MyMux{}
+	server := http.Server{
+		Addr:    ":9090",
+		Handler: router,
+	}
+
+	server.ListenAndServe()
+}
